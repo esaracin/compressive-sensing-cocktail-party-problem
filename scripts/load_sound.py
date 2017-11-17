745debc680cb278f67cca90fc34f0088824df95b
Eli Saracino <esaracin@bu.edu>
2017-11-16 21:57:57 -0500

    updating relative path

--- scripts/load_sound.py
+++ scripts/load_sound.py
@@ -2,5 +2,5 @@ import scipy.io.wavfile as wv
 
 # Simple test script to see how best to load .wav files as vectors.
 
-first, second = wv.read('./elisa/Downloads/disco_dancing.wav')
+first, second = wv.read('../sound_files/disco_dancing.wav')
 print(first, second)
