ebb41d4c04682dbb82f61d36355d0018793289cb
ndhuynh <huynh.n.andy@gmail.com>
2017-11-29 14:44:37 -0500

    Changes format of repository to match final project submission

--- code/code/load_sound.py
+++ code/code/load_sound.py
@@ -4,4 +4,4 @@ import sys
 # Simple test script to see how best to load .wav files as vectors.
 
 first, second = wv.read('../sound_files/disco_dancing.wav')
-print(first, second)
+print(first, second.shape)
