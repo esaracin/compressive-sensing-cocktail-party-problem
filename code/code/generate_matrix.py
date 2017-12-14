f7b077cb4cace523771b2ca05db5a8358817dac8
ndhuynh <huynh.n.andy@gmail.com>
2017-12-14 13:33:12 -0500

    Enhances and cleans generate_matrix script
    Adds verbosity arguments for logging
    Grabs OMP coefficients
    Partitions sub routines into functions
    Takes out redundant computations in main loop

--- code/code/generate_matrix.py
+++ code/code/generate_matrix.py
@@ -10,6 +10,8 @@
 
 import sys
 import glob
+import math
+import logging
 import numpy as np
 import scipy
 import scipy.sparse as sp
@@ -26,61 +28,81 @@ def normalize(v):
     
     return v / norm
 
-def learn_dictionary(N):
-    '''Learns the Sparsifying Dictionary.''' 
-    I = np.eye(N)
-    D = scipy.fftpack.dct(I)
-    return D
-
-    #path = '../data/training/*'
-    #for filename in glob.iglob(path):
-    #    sampling_freq, x = wv.read(filename)
-
-    #return D
-
-
-def main(argv):    
-    if(len(argv) != 2):
-        print('Argument error:\n python soundFile.wav numSources')
-        sys.exit(2)
-
-    # Read in and apply STFT to our audio signal, x.
-    samplingFreq, x = wv.read(argv[0])
-    freqs, segTimes, stft = sig.stft(x, fs=samplingFreq)
+def estimateMixtureCoef(signal, sampFreq, numSources):
+    freqs, segTimes, stft = sig.stft(signal, fs=sampFreq)
     
     # Normalize the time-frequency representation of x
     normalized = np.apply_along_axis(normalize, 0, stft)
     normalized = normalized.flatten('F').T
     
     # Run k-means on column vectors with k = numSources
-    kmeans = KMeans(init='k-means++', n_clusters=int(argv[1]))
+    kmeans = KMeans(init='k-means++', n_clusters=numSources)
     kmeans.fit_predict(normalized.reshape(-1, 1))
     A = kmeans.cluster_centers_.T
 
-    # Use A to construct our mixing matrix, M
+    return A
+
+def constructMixtureMatrix(a, window):
+    first_diag = np.zeros(window)
+    second_diag = np.zeros(window)
+
+    first_diag.fill(a[0][0])
+    second_diag.fill(a[0][1])
+
+    first = sp.diags(first_diag)
+    second = sp.diags(second_diag)
+
+    M = sp.hstack((first, second))
+    
+    return M
+
+def learn_dictionary(N):
+    '''Learns the Sparsifying Dictionary.''' 
+    I = np.eye(N)
+    D = scipy.fftpack.dct(I)
+    return D
+
+def main(argv):    
+    if(len(argv) < 2):
+        print('Argument error:\n python soundFile.wav numSources')
+        sys.exit(2)
+
+    soundFid = argv[0]
+    numSources = int(argv[1])
+
+    # Read in and apply STFT to our audio signal, x.
+    samplingFreq, x = wv.read(soundFid)
+
+
     # Use a windowing procedure so not as to run out of memory
     x = x.reshape(-1, 1)
     l = 500
-    for i in range(70):
-        first_diag = np.zeros(l)
-        second_diag = np.zeros(l)
-    
-        first_diag.fill(A[0][0])
-        second_diag.fill(A[0][1])
+    idx = [x*l for x in range(math.floor(len(x)/l))]
 
-        first = sp.diags(first_diag)
-        second = sp.diags(second_diag)
+    recoveredSources = [np.array([]) for sources in range(numSources)]
 
-        # Combine our matrices to construct M
-        M = sp.hstack((first, second))
-        D = learn_dictionary(M.shape[1]) 
+    A = estimateMixtureCoef(x, samplingFreq, numSources)
 
-        MD = M @ D
+    M = constructMixtureMatrix(A, l)
+    D = learn_dictionary(M.shape[1]) 
+    MD = M@D
+
+    logger.info("Beginning recovery for window:")
+    for i in range(1,len(idx)):
+        logger.debug("\t%d : %d" % (idx[i-1], idx[i]))
         omp = sklearn.linear_model.OrthogonalMatchingPursuit()
-        omp.fit(MD, x[0:l])
-        S = omp.predict(x[0:l])
-        print(S.shape)
-    
+        omp.fit(MD, x[idx[i-1]:idx[i]])
+        coef = omp.coef_
+        for source in range(numSources):
+            np.concatenate((recoveredSources[source], coef[(source*l):(source+1)*l]), axis=0)
 
-if __name__ == '__main__':
+    return 0
+
+if __name__ == '__main__': 
+    if(('-v' in sys.argv) or ('--verbose' in sys.argv)):
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
     main(sys.argv[1:])
