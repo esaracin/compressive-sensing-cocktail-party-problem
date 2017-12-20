ab594af2f4cbe8ffd105ea34d621693d46ab2d84
Eli Saracino <esaracin@bu.edu>
2017-12-20 17:53:41 -0500

    removing unnecessary file

--- code/code/load_sound.py
+++
@@ -1,7 +0,0 @@
-import scipy.io.wavfile as wv
-import sys
-
-# Simple test script to see how best to load .wav files as vectors.
-
-first, second = wv.read('../data/disco_dancing.wav')
-print(first, second.shape)
